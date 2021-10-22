79bec24a046e027bf7ee9b2ac1b1fddce5c1446d
yishengzuiai1128 <33540471+yishengzuiai1128@users.noreply.github.com>
2021-10-23 01:07:12 +0800

    Update unixbench.sh

--- unixbench.sh
+++ unixbench.sh
@@ -30,7 +30,7 @@ if [ -s UnixBench5.1.3.tgz ]; then
     echo "UnixBench5.1.3.tgz [found]"
 else
     echo "UnixBench5.1.3.tgz not found!!!download now..."
-    if ! wget -c https://github.com/yishengzuiai1128/-/blob/main/UnixBench5.1.3.tgz; then
+    if ! wget -c http://byte-unixbench.googlecode.com/files/UnixBench5.1.3.tgz; then
         echo "Failed to download UnixBench5.1.3.tgz, please download it to ${cur_dir} directory manually and try again."
         exit 1
     fi
