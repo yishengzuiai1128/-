e5a79287726e8a98b23f78862dd0ce55c2e9e4da
yishengzuiai1128 <33540471+yishengzuiai1128@users.noreply.github.com>
2021-10-23 01:11:28 +0800

    Update unixbench.sh

--- unixbench.sh
+++ unixbench.sh
@@ -1,44 +1,61 @@
-#! /bin/bash
-#==============================================================#
-#   Description:  Unixbench script                             #
-#   Author: Teddysun <i@teddysun.com>                          #
-#   Intro:  https://teddysun.com/245.html                      #
-#==============================================================#
-cur_dir=/opt/unixbench
+#!/usr/bin/env bash
+#
+# Description: Unixbench script
+# Author: Biubibi
+# Thanks: unixbench.sh <i@teddysun>
+# Intro: http://www.webhk.top/posts/d7ee8401.html
 
-# Check System
-[[ $EUID -ne 0 ]] && echo 'Error: This script must be run as root!' && exit 1
-[[ -f /etc/redhat-release ]] && os='centos'
-[[ ! -z "`egrep -i debian /etc/issue`" ]] && os='debian'
-[[ ! -z "`egrep -i ubuntu /etc/issue`" ]] && os='ubuntu'
-[[ "$os" == '' ]] && echo 'Error: Your system is not supported to run it!' && exit 1
+cur_dir=/opt/unixbench
+UnixBench_file="UnixBench-5.1.4.tar.gz"
+UnixBench_url="https://github.com/thompson1966/scripts/raw/master/tools/UnixBench-5.1.4.tar.gz"
+
+# check root
+[[ $EUID -ne 0 ]] && echo -e "${RED}Error:${PLAIN} This script must be run as root!" && exit 1
+
+# check os
+if [ -f /etc/redhat-release ]; then
+    release="centos"
+elif cat /etc/issue | grep -Eqi "debian"; then
+    release="debian"
+elif cat /etc/issue | grep -Eqi "ubuntu"; then
+    release="ubuntu"
+elif cat /etc/issue | grep -Eqi "centos|red hat|redhat"; then
+    release="centos"
+elif cat /proc/version | grep -Eqi "debian"; then
+    release="debian"
+elif cat /proc/version | grep -Eqi "ubuntu"; then
+    release="ubuntu"
+elif cat /proc/version | grep -Eqi "centos|red hat|redhat"; then
+    release="centos"
+fi
 
 # Install necessary libaries
-if [ "$os" == 'centos' ]; then
+if [ "$release" == 'centos' ]; then
+    yum -y update
     yum -y install make automake gcc autoconf gcc-c++ time perl-Time-HiRes
 else
     apt-get -y update
     apt-get -y install make automake gcc autoconf time perl
 fi
 
-# Create new soft download dir
+# Create new dir
 mkdir -p ${cur_dir}
 cd ${cur_dir}
 
-# Download UnixBench5.1.3
-if [ -s UnixBench5.1.3.tgz ]; then
-    echo "UnixBench5.1.3.tgz [found]"
+# Download UnixBench5.1.4
+if [ -s ${UnixBench_file} ]; then
+    echo "UnixBench5.1.4.tgz [found]"
 else
-    echo "UnixBench5.1.3.tgz not found!!!download now..."
-    if ! wget -c http://byte-unixbench.googlecode.com/files/UnixBench5.1.3.tgz; then
-        echo "Failed to download UnixBench5.1.3.tgz, please download it to ${cur_dir} directory manually and try again."
+    echo "UnixBench5.1.4.tgz not found!!!download now..."
+    if ! wget -c ${UnixBench_url}; then
+        echo "Failed to download UnixBench5.1.4.tgz, please download it to ${cur_dir} directory manually and try again."
         exit 1
     fi
 fi
-tar -zxvf UnixBench5.1.3.tgz && rm -f UnixBench5.1.3.tgz
+tar -zxvf ${UnixBench_file} && rm -f ${UnixBench_file}
 cd UnixBench/
 
-#Run unixbench
+# Run unixbench
 make
 ./Run
 
